57bc40bf20c8f2dc981d458853f6bc6ee4dd9127
Uladzislau Svintsitski <v.v.svintsitsky@gmail.com>
2024-07-29 15:07:27 +0200

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './app/components/header/header.component';
+import { LogoComponent } from './app/components/logo/logo.component';
+import { FooterComponent } from './app/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
