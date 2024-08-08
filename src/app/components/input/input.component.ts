67558af296d7f0ce2c0cebdd9ac1b7bd4a86928e
Uladzislau_Svintsitski <uladzislau_svintsitski@epam.com>
2024-08-08 18:05:01 +0200

    implement custom input control

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,17 +1,62 @@
-import { Component, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() placeholder = '';
 
-  @Input({ required: true }) formControlName!: string;
-
   @Input() required = false;
+
+  private _value = '';
+
+  get value() {
+    return this._value;
+  }
+
+  set value(_value: string) {
+    this._value = _value;
+    this.onChange(_value);
+  }
+
+  isDisabled = false;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  onChange(_: string) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  handleBlur() {}
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.handleBlur = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
 }
